d70403d08cf2100a5b01344d9e28ae628bab9d8a
wuyikai <wuyikai@secoo.com>
2017-06-27 17:56:57 +0800

    使用runloop优化

--- async-display/Demo/Demo2ViewController.m
+++ async-display/Demo/Demo2ViewController.m
@@ -9,21 +9,51 @@
 #import "Demo2ViewController.h"
 #import "STMFPSLabel.h"
 
+typedef BOOL(^Task)(void);
+
 @interface Demo2ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+@property (nonatomic, weak) NSTimer *timer;
+@property (nonatomic, strong) NSMutableArray<Task> *tasks;
+@property (nonatomic, assign) NSInteger maxQueueLength;
+
 @end
 
+//回调函数
+static void Callback(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info){
+    Demo2ViewController * vc = (__bridge Demo2ViewController *)info;
+    if (vc.tasks.count == 0) {
+      return;
+    }
+    BOOL result = NO;
+    while (result == NO && vc.tasks.count) {
+      //取出任务
+      Task unit = vc.tasks.firstObject;
+      //执行任务
+      result = unit();
+      //干掉第一个任务
+      [vc.tasks removeObjectAtIndex:0];
+    }
+}
+
 @implementation Demo2ViewController
+
+- (void)dealloc {
+  [_timer invalidate];
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   [self.tableView registerClass:[Demo2Cell class] forCellReuseIdentifier:NSStringFromClass([Demo2Cell class])];
-
   STMFPSLabel *fps = [[STMFPSLabel alloc] initWithFrame:CGRectMake(5, 64, 0, 0)];
   [self.view addSubview:fps];
   [fps sizeToFit];
+
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(_timerAction) userInfo:nil repeats:YES];
+  [self _addRunloopObserver];
+  self.maxQueueLength = 18;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -39,9 +69,61 @@
 - (void)_configureCell:(Demo2Cell *)cell atIndexPath:(NSIndexPath *)indexPath {
   [cell reset];
   [cell addLabel:indexPath];
-  [cell addImage1];
-  [cell addImage2];
-  [cell addImage3];
+//  [cell addImage1];
+//  [cell addImage2];
+//  [cell addImage3];
+
+  [self _addTask:^BOOL{
+    [cell addImage1];
+    return YES;
+  }];
+  [self _addTask:^BOOL{
+    [cell addImage2];
+    return YES;
+  }];
+  [self _addTask:^BOOL{
+    [cell addImage3];
+    return YES;
+  }];
+}
+
+- (void)_addTask:(Task)unit {
+  [self.tasks addObject:unit];
+  //判断一下 保证没有来得及显示的cell不会绘制图片!!
+  if (self.tasks.count > self.maxQueueLength) {
+    [self.tasks removeObjectAtIndex:0];
+  }
+}
+
+- (void)_addRunloopObserver {
+  //获取当前RunLoop
+  CFRunLoopRef runloop = CFRunLoopGetCurrent();
+  //定义一个上下文
+  CFRunLoopObserverContext context = {
+    0,
+    (__bridge void *)(self),
+    &CFRetain,
+    &CFRelease,
+    NULL,
+  };
+  //定义一个观察者
+  static CFRunLoopObserverRef defaultModeObserver;
+  //创建观察者
+  defaultModeObserver = CFRunLoopObserverCreate(NULL, kCFRunLoopBeforeWaiting, YES, NSIntegerMax - 999, &Callback, &context);
+  //添加当前RunLoop的观察者
+  CFRunLoopAddObserver(runloop, defaultModeObserver, kCFRunLoopDefaultMode);
+  CFRelease(defaultModeObserver);
+}
+
+- (void)_timerAction {}
+
+#pragma mark - setter & getter
+
+- (NSMutableArray<Task> *)tasks {
+  if (!_tasks) {
+    _tasks = [NSMutableArray<Task> array];
+  }
+  return _tasks;
 }
 
 @end
@@ -78,8 +160,12 @@ static NSInteger const kTag = 1000;
 }
 
 - (void)addImage1 {
+  NSInteger tag = 1 + kTag;
+  if ([self.contentView viewWithTag:tag]) {
+    return;
+  }
   UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(5, 20, 85, 85)];
-  imageView.tag = 1 + kTag;
+  imageView.tag = tag;
   UIImage *image = [UIImage imageNamed:@"spaceship2"];
   imageView.contentMode = UIViewContentModeScaleAspectFit;
   imageView.image = image;
@@ -89,8 +175,10 @@ static NSInteger const kTag = 1000;
 }
 
 - (void)addImage2 {
+  NSInteger tag = 2 + kTag;
+  if ([self.contentView viewWithTag:tag]) { return; }
   UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(105, 20, 85, 85)];
-  imageView1.tag = 2 + kTag;
+  imageView1.tag = tag;
   UIImage *image1 = [UIImage imageNamed:@"spaceship2"];
   imageView1.contentMode = UIViewContentModeScaleAspectFit;
   imageView1.image = image1;
@@ -100,8 +188,10 @@ static NSInteger const kTag = 1000;
 }
 
 - (void)addImage3 {
+  NSInteger tag = 3 + kTag;
+  if ([self.contentView viewWithTag:tag]) { return; }
   UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(200, 20, 85, 85)];
-  imageView2.tag = 3 + kTag;
+  imageView2.tag = tag;
   UIImage *image2 = [UIImage imageNamed:@"spaceship2"];
   imageView2.contentMode = UIViewContentModeScaleAspectFit;
   imageView2.image = image2;
